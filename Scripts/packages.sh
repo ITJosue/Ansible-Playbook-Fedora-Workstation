8daf21dc91ad3fff012ad58b3cb790afd058d08b
Josue <118592169+ITJosue@users.noreply.github.com>
2026-03-14 01:20:19 -0500

    Update package list in packages.sh

--- Scripts/packages.sh
+++ Scripts/packages.sh
@@ -6,13 +6,14 @@ dnf install -y \
     clementine \
     gh git-lfs godot nix nix-daemon \
     prusa-slicer \
-    kate kmousetool krename krusader ktimer kcron kclock dolphin-plugins ksystemlog kompare nextcloud-client-dolphin \
+    kate kmousetool krename krdc krusader ktimer kcron kclock dolphin-plugins ksystemlog kompare nextcloud-client-dolphin \
     buildah flatpak-builder skopeo \
-    gnome-boxes 'incus-*' libguestfs libguestfs-tools-c vagrant vagrant-libvirt ansible ansible-collection-community-general 'podman*' swtpm qemu-img \
+    gnome-boxes 'incus-*' libguestfs libguestfs-tools-c ansible ansible-collection-community-general 'podman*' swtpm qemu-img \
     'cockpit-*' \
     protonvpn-stable-release proton-vpn-gnome-desktop proton-vpn-cli \
     fail2ban fail2ban-firewalld fail2ban-systemd \
     zoom \
-    google-noto-fonts-all google-noto-color-emoji-fonts rsms-inter-fonts
+    google-noto-fonts-all google-noto-color-emoji-fonts rsms-inter-fonts \
+    packer vagrant vagrant-libvirt #hashicorp
 
 exit 0
