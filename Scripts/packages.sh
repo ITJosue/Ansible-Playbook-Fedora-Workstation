81bd80ec2ac0663c693baf410ae6b7c0c5f27342
itjosue <118592169+ITJosue@users.noreply.github.com>
2026-08-31 20:59:03 -0500

    Sync offline changes

--- Scripts/packages.sh
+++ Scripts/packages.sh
@@ -10,19 +10,20 @@ echo "Starting package installation. Logging to ${LOG_FILE}..."
 
 dnf install -y \
     @virtualization \
-    7zip android-tools bleachbit calibre conda corectrl displaycal distrobox fastfetch fish gtk3-devel libva-utils pidgin pidgin-otr snapd tldr zsh \
+    7zip android-tools bleachbit calibre conda corectrl DisplayCAL distrobox fastfetch fish gtk3-devel libva-utils pidgin pidgin-otr snapd strace tldr tmux uv zsh \
     google-chrome-stable microsoft-edge-stable \
     clementine \
-    gh git-lfs godot nix nix-daemon \
+    gh git-lfs godot nix nix-daemon ollama \
     prusa-slicer \
     kate kdegraphics-thumbnailers 'kio*' kmousetool krename krdc krusader ktimer kcron kclock dolphin-plugins ksystemlog kompare nextcloud-client-dolphin \
     buildah flatpak-builder skopeo \
-    ansible ansible-collection-community-general gnome-boxes nbdkit 'incus-*' 'libguestfs*'  'podman*' swtpm qemu-img \
+    ansible ansible-collection-community-general gnome-boxes nbdkit 'incus-*' 'libguestfs*' 'podman*' podman-compose podman-docker podman-machine podman-remote podman-tui swtpm qemu-img \
     'cockpit-*' \
     protonvpn-stable-release proton-vpn-gnome-desktop proton-vpn-cli \
     fail2ban fail2ban-firewalld fail2ban-systemd \
+    wireguard-tools nmap-ncat \
     zoom \
     google-noto-fonts-all google-noto-color-emoji-fonts rsms-inter-fonts \
-    boundary packer terraform vagrant vagrant-libvirt vagrant-cachier vagrant-sshfs vagrant-hostmanager vagrant-registration waypoint \
+    boundary packer terraform terraform-ls terraform-stacks-cli vagrant vagrant-libvirt vagrant-cachier vagrant-sshfs vagrant-hostmanager vagrant-registration waypoint \
     ffmpegthumbnailer
 exit 0
